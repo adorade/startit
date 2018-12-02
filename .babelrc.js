471676d46a99947e4734e3bcd14286a026f7b0dd
Marian Bolojan <cretu.adorade@gmail.com>
2018-12-02 18:20:43 +0200

    Use .babelrc.js for Babel configuration

---
+++ .babelrc.js
@@ -0,0 +1,17 @@
+/*!
+ * Startit (v1.0.0): .babelrc.js
+ * Copyright (c) 2017 - 2018  Adorade (https://adorade.ro)
+ * Licensed under MIT
+ * ============================================================================
+ */
+
+ const presets = [
+  '@babel/env'
+];
+const plugins = [
+  'add-module-exports'
+];
+
+module.exports = {
+  presets, plugins
+};
